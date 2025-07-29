7683cf6816d07e3a337ec076cdd52d9b60c5aa17
prabhakar <prabhakarpandey7061@gmail.com>
2025-07-29 16:14:40 +0530

    fix error

--- em-project/src/main/java/com/example/em_project/EmpController.java
+++ em-project/src/main/java/com/example/em_project/EmpController.java
@@ -1,17 +1,33 @@
 package com.example.em_project;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.eclipse.persistence.testing.oxm.xmlmarshaller.structureValidation.sequence.Employee;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 public class EmpController {
+ List<Employee>empoloyees = new ArrayList<>();
+
     @GetMapping("employees")
-    public List<Employeee> getAllEmployees()    {
-        List<Employee> employess = new ArrayList<>();
-        return employess;
+    public List<Employee> getAllEmpoloyees() { 
+        empoloyees.add(null);
+        return empoloyees;
     }
+
+    @PostMapping("employees")
+    public String postMethodName(@RequestBody Employee employee) {
+
+        empoloyees.add(employee);
+        return "Employee added successfully";
+
+    }
+    
+    
     
 }
---
+++ em-project/src/main/java/com/example/em_project/Employee.java
@@ -0,0 +1,19 @@
+package com.example.em_project;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private  String name;
+    private String phone;
+    private String email;
+
+  
+
+
+}
+
