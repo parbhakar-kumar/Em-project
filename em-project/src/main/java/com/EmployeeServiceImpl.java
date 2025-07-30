ea635d6c0ca99ad1d07dc63ec28e168c0982a3ed
prabhakar <prabhakarpandey7061@gmail.com>
2025-07-30 14:41:59 +0530

    fix error in EmpController.java

--- em-project/src/main/java/com/EmployeeServiceImpl.java
+++ em-project/src/main/java/com/EmployeeServiceImpl.java
@@ -7,15 +7,20 @@ import com.example.em_project.EmployeeService;
 
 public abstract class EmployeeServiceImpl implements EmployeeService {
 
+
+
+       @Override
+    public Employee ceratEmployees(Employee employee) {
+       EmployeeService.add(employee);
+        return employee;
+    }
+
+    
     @Override
     public List<Employee> readEmployees() {
         // TODO: implement method logic
         return null;
     }
 
-    @Override
-    public Employee ceratEmployees(Employee employee) {
-       EmployeeService.add(employee);
-        return employee;
-    }
+ 
 }
--- em-project/src/main/java/com/example/em_project/EmpController.java
+++ em-project/src/main/java/com/example/em_project/EmpController.java
@@ -1,7 +1,5 @@
 package com.example.em_project;
 
-import java.util.ArrayList;
-
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,25 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmpController {
- List<Employee>empoloyees = new ArrayList<>();
 //  List<Employee>empoloyees = new ArrayList<>();
 EmployeeService employeeService;
 
     @GetMapping("employees")
     public List<Employee> getAllEmpoloyees() { 
-        empoloyees.add(null);
-        return empoloyees;
-        // empoloyees.add(null);
         return employeeService.readEmployees();
     }
 
     @PostMapping("employees")
     public String postMethodName(@RequestBody Employee employee) {
-
-        empoloyees.add(employee);
-        return "Employee added successfully";
-        // empoloyees.add(employee);
-       return  employeeService.ceratEmployee(employee);
+        employeeService.createEmployee(employee);
+        
+        return "seved successfully";
+        
         
 
     }
